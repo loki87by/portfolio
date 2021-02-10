23497d8c82b3294583dbaf58bfc36ffa226476c4
Alexey Akulich <loki1987by@yandex.ru>
2021-02-10 19:26:56 +0300

    добавить кнопку рестарта игры

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,6 +1,7 @@
 // **импорты
 import React from 'react';
 import { TranslationContext } from '../../contexts/translationContext';
+import { waveAnimator } from '../../utils/consts';
 import GameField from '../GameField/GameField';
 import Popup from '../Popup/Popup';
 import './Game.css';
@@ -10,6 +11,8 @@ import './styles/__text-container/Game__text-container.css';
 import './styles/__rules-title/Game__rules-title.css';
 import './styles/__rules-text/Game__rules-text.css';
 import './styles/__rules-span/Game__rules-span.css';
+import './styles/__restart-button/Game__restart-button.css';
+import './styles/__restart-button/Game__restart-button_symbol.css';
 import './styles/__rules-text/_gameStarted/Game__rules-text_gameStarted.css';
 import './styles/__rules-title/_gameStarted/Game__rules-title_gameStarted.css';
 
@@ -19,6 +22,23 @@ function Game(props) {
   const translation = React.useContext(TranslationContext);
   const [scrollLocker, setScrollLocker] = React.useState(false);
 
+  React.useEffect(() => {
+    const button = document.querySelector('.Game__restart-button')
+    if(button){
+      function startAnimation() {
+        button.innerHTML = waveAnimator(translation.restart)
+      }
+      function overAnimation() {
+        button.innerHTML = translation.restart
+      }
+      button.addEventListener('mouseover', startAnimation);
+      button.addEventListener('mouseout', overAnimation);
+      return () => {
+        button.removeEventListener('mouseover', startAnimation);
+        button.removeEventListener('mouseout', overAnimation);
+      };
+    }
+  })
   // *кнопка переключения скролла свайпом
   function scrollSwitcher() {
     if(scrollLocker) {
@@ -57,7 +77,10 @@ function Game(props) {
         <p className={`Game__rules-text ${props.gameStarted && "Game__rules-text_gameStarted"}`}>{props.isMobile ? translation.mobileRules : translation.rules}</p>
         {props.endGame ?
           props.gameStarted ? ''
-            : <span className="Game__rules-span">{translation.gameOver}</span>
+            : <>
+                <span className="Game__rules-span">{translation.gameOver}</span>
+                <button className="Game__restart-button" type="button" onClick={props.restarter}>{translation.restart}</button>
+              </>
           : props.gameStarted ? ''
             : <span className="Game__rules-span">{props.isMobile ? translation.prestartMobile : translation.prestart}</span>}
       </div>

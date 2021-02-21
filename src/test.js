d181ca224a246e58fd548e38bd9e98ff046171de
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-02-21 23:28:01 +0300

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,3 +1,5 @@
+function superscript() {
 let u='scripts/XML_daily', d='.asp?date_req=17/02/2021', w=location+u+d, f=()=>{
   return fetch(w, {headers: {'Content-Type': 'application/xml'}})
   .then(res => console.log(res.data))};
+}

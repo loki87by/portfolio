938f2459ca34192f8e2f1218752821d9c55df21c
Alexey Akulich <loki1987by@yandex.ru>
2022-01-15 17:55:28 +0300

    fix: finish footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,15 @@
 import React from "react";
 import { TranslationContext } from "../../contexts/translationContext";
 import "./Footer.css";
+import "./styles/Footer__link.css";
 
 // **функционал
 function Footer() {
   const translation = React.useContext(TranslationContext);
   return (
     <footer className="Footer">
-      <p>&copy; 2020 {translation.author}</p>
+      <a href="https://github.com/loki87by" className="Footer__link">&copy; </a>
+      <p>2020 {translation.author}</p>
     </footer>
   );
 }
